1325621d219cf8d1e81f4404a6ec02b6f8d6ec01
ふる <furusample@example.com>
2026-01-30 14:04:24 +0900

    ∞モードのボタンの色を変更・問題追加

--- infinityData.js
+++ infinityData.js
@@ -19,6 +19,33 @@ window.infinityProblems = [
       m: { min: 2, max: 12 }
     },
     formula: (v) => v.F / v.m
+  },
+
+  {
+    template: "An object with mass {m} kg moves at a velocity of {v} m/s. Calculate its kinetic energy.",
+    vars: {
+      m: { min: 1, max: 10 },
+      v: { min: 5, max: 20 }
+    },
+    formula: (v) => 0.5 * v.m * v.v * v.v
+  },
+
+  {
+    template: "An object is lifted to a height of {h} m with a mass of {m} kg. Find the gravitational potential energy. (g = 10 m/s²)",
+    vars: {
+      m: { min: 1, max: 10 },
+      h: { min: 2, max: 20 }
+    },
+    formula: (v) => v.m * 10 * v.h
+  },
+
+  {
+    template: "A force of {F} N is applied over a distance of {d} m. Calculate the work done.",
+    vars: {
+      F: { min: 10, max: 50 },
+      d: { min: 2, max: 15 }
+    },
+    formula: (v) => v.F * v.d
   }
 
 ];
